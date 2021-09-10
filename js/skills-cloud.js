ea083b8257453ea0d0d9efc8ef71c0a3acca7378
Juergen-J <yurk.jur@gmail.com>
2021-09-10 21:32:55 +0200

    index updated

--- js/skills-cloud.js
+++ js/skills-cloud.js
@@ -2,21 +2,22 @@
 let word_list = [
     {text: "Java", weight: 13, link: "#skills"},
     {
-        text: "JavaScript",  weight: 10.5,   html: {title: "My Title", "class": "custom-class"},
+        text: "JavaScript", weight: 10.5, html: {title: "My Title", "class": "custom-class"},
         link: {href: "#skills"}
     },
     {text: "Gradle", weight: 9.4},
     {text: "Maven", weight: 8},
-    {text: "Spring-Boot", weight: 6.2},
-    {text: "Rest-services", weight: 6.2},
+    {text: "Spring Boot", weight: 6.2},
+    {text: "Spring Web", weight: 6.2},
     {text: "JUnit", weight: 6.2},
-    {text: "JPA Hibernate", weight: 6.2},
+    {text: "Spring Data (JPA Hibernate)", weight: 6.2},
     {text: "Spring MVC", weight: 7},
     {text: "Git", weight: 7},
     {text: "PostgresSQL", weight: 5},
     {text: "Jenkins", weight: 5},
     {text: "Docker", weight: 5},
     {text: "HTML", weight: 5},
+    {text: "Mockito", weight: 5},
     {text: "CSS", weight: 5}
 ];
 $(function () {
