7064122b3bf31a7a19d67759d8a2967be616259e
JJ <yurk.jur@gmail.com>
2025-01-06 21:21:40 +0100

    base update

--- js/skills-cloud.js
+++ js/skills-cloud.js
@@ -1,25 +1,26 @@
 // создание массива тегов
 let word_list = [
     {text: "Java", weight: 13, link: "#skills"},
-    {
-        text: "JavaScript", weight: 10.5, html: {title: "My Title", "class": "custom-class"},
-        link: {href: "#skills"}
-    },
-    {text: "Gradle", weight: 9.4},
-    {text: "Maven", weight: 8},
-    {text: "Spring Boot", weight: 6.2},
-    {text: "Spring Web", weight: 6.2},
-    {text: "JUnit", weight: 6.2},
-    {text: "Spring Data (JPA Hibernate)", weight: 6.2},
-    {text: "Spring MVC", weight: 7},
-    {text: "Git", weight: 7},
-    {text: "PostgresSQL", weight: 5},
-    {text: "Jenkins", weight: 5},
-    {text: "Docker", weight: 5},
-    {text: "HTML", weight: 5},
-    {text: "Mockito", weight: 5},
-    {text: "CSS", weight: 5}
+    {text: "JavaScript", weight: 10.5, link: "#skills"},
+    {text: "TypeScript", weight: 10, link: "#skills"},
+    {text: "Spring Boot", weight: 9.5, link: "#skills"},
+    {text: "Spring MVC", weight: 9.3, link: "#skills"},
+    {text: "Spring Data (JPA/Hibernate)", weight: 9, link: "#skills"},
+    {text: "Angular", weight: 8.8, link: "#skills"},
+    {text: "HTML", weight: 8.5, link: "#skills"},
+    {text: "CSS", weight: 8.5, link: "#skills"},
+    {text: "Docker", weight: 8, link: "#skills"},
+    {text: "Git", weight: 7.8, link: "#skills"},
+    {text: "PostgreSQL", weight: 7.5, link: "#skills"},
+    {text: "MySQL", weight: 7.3, link: "#skills"},
+    {text: "JUnit", weight: 7, link: "#skills"},
+    {text: "Mockito", weight: 6.8, link: "#skills"},
+    {text: "Gradle", weight: 6.5, link: "#skills"},
+    {text: "Maven", weight: 6.3, link: "#skills"},
+    {text: "AWS", weight: 6, link: "#skills"},
+    {text: "Bootstrap", weight: 5.5, link: "#skills"}
 ];
+
 $(function () {
     $("#skills-cloud").jQCloud(word_list);
 });
--- js/text-animation.js
+++ js/text-animation.js
@@ -1,15 +1,12 @@
-let TEXTS = ["Frontend developer", "Backend developer", "Creative Person"];
+let TEXTS = ["Frontend developer", "Backend developer", "DevOPS"];
 
 let index = 0;
 $(function () {
     setInterval(function () {
         $('#header-text-change').fadeOut(1000, function () {
-            // 1500
             $(this).text(TEXTS[index++]).fadeIn(900);
-// 2000
             if (index === TEXTS.length)
                 index = 0
         });
     }, 0);
-//    3000
 });
